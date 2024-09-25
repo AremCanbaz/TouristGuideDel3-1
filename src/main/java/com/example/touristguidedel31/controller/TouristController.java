c3b8e3ce9b267a22360ae3533dfbdc01021e955b
AC25-C <arem_canbaz@icloud.com>
2024-09-25 15:16:13 +0200

    test

--- src/main/java/com/example/touristguidedel31/controller/TouristController.java
+++ src/main/java/com/example/touristguidedel31/controller/TouristController.java
@@ -14,7 +14,6 @@ import java.util.Set;
 @Controller
 public class TouristController {
 
-    @Autowired
     private TouristRepository touristRepository;
 
     private final TouristService touristService;
